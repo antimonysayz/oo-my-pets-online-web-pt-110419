330b0370f579d3b7f4f291decea70aaa5994f323
Anthony Mai <anthony.mai530@gmail.com>
2019-12-25 01:38:14 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   @@all = []
-  def initialize(name, owner)
+  def initialize(name)
     @name = name
     @owner = owner
     @mood = "nervous"
