6a97b7f69f031986c3101b4cb8335f36f03844b2
Anthony Mai <anthony.mai530@gmail.com>
2019-12-25 01:52:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,13 +50,13 @@ class Owner
   end
   
   def walk_dogs()
-    @@pets[:dogs].each do |dog|
+    pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
   
   def feed_cats()
-    @@pets[:cats].each do |cat|
+    pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
