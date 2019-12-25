ff0b507f7818f97a2fd56df36255e2d0d1c28111
Anthony Mai <anthony.mai530@gmail.com>
2019-12-25 01:35:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :pets, :fish, :dog, :cat, :mood
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+  
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+  
+  def pets 
+    @@pets
+  end
+  
+  def walk_dogs()
+    @@pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def feed_cats()
+    @@pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def list_pets
+    return "I have #{@@pets[:dogs].length} dog(s), and #{@@pets[:cats].length} cat(s)."
+  end
+  
+  def sell_pets
+    pets.each do |pet, pets|
+      pets.map do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+      end
+      pets.clear
+    end
+  end
+  
 end
